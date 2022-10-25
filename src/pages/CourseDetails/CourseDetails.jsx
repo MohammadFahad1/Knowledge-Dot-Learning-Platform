e602b6b6681ba9d92e058a9a021bc0bf0727272a
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-10-26 00:48:05 +0600

    Course Details Page Added with Modules

--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Module from './Module/Module';
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData();
     const { course, modules } = JSON.parse(courseDetails);
-    console.log(courseDetails);
     return (
         <div className='pt-14 bg-slate-400'>
             <div className="mx-auto max-w-5xl px-4 py-8">
@@ -33,70 +33,12 @@ const CourseDetails = () => {
             </div>
 
 
-            <div class="space-y-4">
-                <details class="group" open>
-                    <summary
-                        class="flex cursor-pointer items-center justify-between rounded-lg bg-gray-50 p-4"
-                    >
-                        <h2 class="font-medium text-gray-900">
-                            Lorem ipsum dolor sit amet consectetur adipisicing?
-                        </h2>
-
-                        <svg
-                            class="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M19 9l-7 7-7-7"
-                            />
-                        </svg>
-                    </summary>
-
-                    <p class="mt-4 px-4 leading-relaxed text-gray-700">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                        molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                        voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                        earum similique!
-                    </p>
-                </details>
-
-                <details class="group">
-                    <summary
-                        class="flex cursor-pointer items-center justify-between rounded-lg bg-gray-50 p-4"
-                    >
-                        <h2 class="font-medium text-gray-900">
-                            Lorem ipsum dolor sit amet consectetur adipisicing?
-                        </h2>
-
-                        <svg
-                            class="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M19 9l-7 7-7-7"
-                            />
-                        </svg>
-                    </summary>
-
-                    <p class="mt-4 px-4 leading-relaxed text-gray-700">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                        molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                        voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                        earum similique!
-                    </p>
-                </details>
+            <div className="mx-5">
+                <div className="space-y-4 py-5 mx-auto" style={{ maxWidth: '900px' }}>
+                    {
+                        modules.map((module) => <Module key={module.id} module={module}></Module>)
+                    }
+                </div>
             </div>
 
 
