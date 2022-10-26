9d4ac41ccf1d46bf00da41d796d21f73e8461c30
Office PC ABA (Fahad) <abasc1962@gmail.com>
2022-10-26 17:45:57 +0600

    Button Added Purchase Now

--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -40,7 +40,7 @@ const CourseDetails = () => {
 
                     <div className='flex flex-col md:flex-row items-center md:justify-between'>
                         <div></div>
-                        <button type="button" class="text-gray-900 text-2xl font-extrabold bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 font-bold">Purchase Premium</button>
+                        <button type="button" class="text-gray-900 text-2xl font-extrabold bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 rounded-lg px-5 py-2.5 text-center mr-2 mb-2">Purchase Premium</button>
                     </div>
                 </div>
             </div>
